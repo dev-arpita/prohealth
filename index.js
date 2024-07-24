427d3915e4f5f8321c5e0a532b9b32cb3b14ed68
Arpita Roy <arpita.roy.workar@gmail.com>
2024-07-24 09:47:31 +0530

    wip : mobile-view menu-btn

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 
+const mobileMenu = document.getElementById("mobile-menu")
+document.getElementById("menu-btn").addEventListener("click", ()=> {
+  mobileMenu.style.display = ("block")
+})
 
-document.getElementById("menu-btn").addEventListener("click", ()=> console.log("clicked"))
-
-document.getElementById("close-btn").addEventListener("click", ()=> console.log("closed"))
-
-const getMenuBarHtml = ()=> {
-  return   `
-    <ul class="sm:hidden sm:flex flex-col gap-4 self-center text-sm lg:text-base">
-        <li class="drop-shadow-xl border-b-2 border-black font-bold"><a href="#">Home</a></li>
-        <li class=""><a href="">About</a></li>
-        <li class=""><a href="/doctors-list.html">Find Doctor</a></li>
-        <li class=""><a href="/blog.html">Blog</a></li>
-        <li class=""><a href="">Contact</a></li>
-    </ul>
-  
-  `
-}
-console.log(getMenuBarHtml())
+document.getElementById("close-btn").addEventListener("click", ()=> {
+  mobileMenu.style.display = ("none")
+})
